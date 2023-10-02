0ca94396bef7ad641b0f9c12d531d5750ca7fbae
andriikovall <anderkoval3@gmail.com>
2023-10-02 19:59:38 +0300

    feat: init proj

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+type T = {
+    name: string;
+    age: number;
+}
+
+const t: T = {
+    name: 'name',
+    age: 18
+}
+console.log(t);
+
+export { }
---
+++ src/types.ts
@@ -0,0 +1,44 @@
+export type Input = {
+  workers: Worker[];
+  tasks: Task[];
+};
+
+export type Output = {
+  /**
+   * The list of tasks that each worker must do with a minimal possible duration
+   * 
+   * Each task will surely we completed and the time will be minimal
+   */
+  result: Record<Worker['id'], Task[]>;
+};
+
+export const PRIORITIES = {
+  Low: 1,
+  Medium: 2,
+  High: 3,
+  Critical: 4,
+} as const;
+
+export type Task = {
+  id: string;
+  name: string;
+  /**
+   * Any relative number, e.g. 1, 2, 3, 4, 5, 10, 100, 1000.
+   * Can be treated as a relative number of hours, days, whatever
+   */
+  duration: number;
+  /**
+   * Tasks that must be done before this task
+   */
+  dependsOn: string[];
+  /**
+   * Workers who can do this task
+   */
+  canBeDoneBy: string[];
+  priority: number;
+};
+
+export type Worker = {
+  id: string;
+  name: string;
+};
