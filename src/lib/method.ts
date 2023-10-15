0f8dc989db21c4b334f177c8e6783b2fb1fdc833
andriikovall <anderkoval3@gmail.com>
2023-10-15 14:34:32 +0300

    docs: added todo for input validation

--- src/lib/method.ts
+++ src/lib/method.ts
@@ -2,5 +2,12 @@ import { Input, Output } from '../types';
 import { brutForce } from './brutForce';
 
 export const method = (input: Input): Output => {
+  // todo: Add validation of input data here
+  /**
+   * A worker can't perform any tasks
+   * A task can't be performed by any worker
+   * Empty list of workers or tasks: How the function behaves with no data. 
+   * Only one worker and multiple tasks, or only one task and multiple workers: This will test the extremes of a single row or single column.
+   */
   return brutForce(input);
 };
