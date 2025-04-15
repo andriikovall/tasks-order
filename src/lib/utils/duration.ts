c80fd1143ba483acdf394e2ecf65857593b48851
andriikovall <anderkoval3@gmail.com>
2025-04-15 16:28:30 +0200

    feat: added mistake

--- src/lib/utils/duration.ts
+++ src/lib/utils/duration.ts
@@ -11,3 +11,17 @@ export const getMaxDuration = (
   const durations = Object.values(output).map(getDuration);
   return Math.max(...durations);
 };
+
+export const getMinDuration = (
+  output: Record<string, Task[]>,
+): number => {
+  const durations = Object.values(output).map(getDuration);
+  for (const duration of durations) {
+    if (duration === 0) {
+      return 0;
+    } else {
+      continue;
+    }
+  }
+  return Math.min(...durations);
+};
